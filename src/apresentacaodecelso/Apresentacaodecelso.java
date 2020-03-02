e148363b8e5ebc302cdd289342d2e1a72e42aa95
Aluno <Aluno@C302-LABC4-001>
2020-03-02 13:47:47 -0300

    escrevi uma msgm de bomdia

--- src/apresentacaodecelso/Apresentacaodecelso.java
+++ src/apresentacaodecelso/Apresentacaodecelso.java
@@ -15,6 +15,7 @@ public class Apresentacaodecelso {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        System.out.println("Bomdia");
         // TODO code application logic here
     }
     
